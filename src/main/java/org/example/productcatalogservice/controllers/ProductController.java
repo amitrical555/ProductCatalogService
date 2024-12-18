76ccb508e1aa7c4556db8fc981a2b81d334129c8
Amit Gupta -R3577D <Amit.Gupta@Securemeters.Com>
2024-12-18 18:57:12 +0530

    First POST API added

--- src/main/java/org/example/productcatalogservice/controllers/ProductController.java
+++ src/main/java/org/example/productcatalogservice/controllers/ProductController.java
@@ -2,17 +2,21 @@ package org.example.productcatalogservice.controllers;
 
 import org.example.productcatalogservice.models.Category;
 import org.example.productcatalogservice.models.Product;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
 
+// @Controller, @Service etc. are subtypes of @Component
+// @RestController is subtype of @Controller
+// @RestController is best used when you want to return data
+// (e.g. JSON or XML) rather than a view (HTML).
 @RestController
 @RequestMapping("/products")
 public class ProductController {
+    // @GetMapping for HttpMethod GET and similar for others
+    // 'Spring by default shows 'White label error' page, when there is 404 or 500 error.
+
     @GetMapping
     public List<Product> getAllProducts() {
         Product product1 = new Product();
@@ -43,11 +47,18 @@ public class ProductController {
         return products;
     }
 
+    // If it is {productId} in path, then use @PathVariable("productId")
     @GetMapping("/{id}")
     public Product findProductById(@PathVariable Long id) {
         Product product = new Product();
         product.setId(id);
         product.setName("Product");
+        return product;
+    }
+
+    // Generally Post should return the object created, rather than void / bool etc.
+    @PostMapping
+    public Product createProduct(@RequestBody Product product) {
         return product;
     }
 }
--- src/main/java/org/example/productcatalogservice/models/BaseModel.java
+++ src/main/java/org/example/productcatalogservice/models/BaseModel.java
@@ -11,5 +11,6 @@ public abstract class BaseModel {
     private Long id;
     private Date createdAt;
     private Date lastUpdatedAt;
+    // Data is actually never deleted, ony state is updated to filter
     private State state;
 }
